7a300cbc4d0849503ec945e22bc339d24e13afbd
lisanyny <110669469+lisanyny@users.noreply.github.com>
2022-10-25 10:38:34 +0000

    [Update]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,16 +5,20 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def create
-    @genre = Genre.new
+    @genre = Genre.new(genre_params)
     @genres = Genre.all
     @genre.save
     redirect_to admin_genres_path
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   private
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,38 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @items = Item.all
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genre = @item.genre
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
 end
